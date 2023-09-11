6b01ee6cb4061524193d01c0e845f7c4c0deb615
kamsansari007 <92922613+kamsansari007@users.noreply.github.com>
2023-09-11 12:26:17 +0530

    Update AdminOrders.js
    base url

--- src/pages/admin/AdminOrders.js
+++ src/pages/admin/AdminOrders.js
@@ -21,7 +21,7 @@ const AdminOrders = () => {
   const [auth, setAuth] = useAuth();
   const getOrders = async () => {
     try {
-      const { data } = await axios.get("/api/v1/auth/all-orders");
+      const { data } = await axios.get("https://mern-backend-bu3c.onrender.com/api/v1/auth/all-orders");
       console.log("data", data);
       setOrders(data);
     } catch (error) {
@@ -35,7 +35,7 @@ const AdminOrders = () => {
 
   const handleChange = async (orderId, value) => {
     try {
-      const { data } = await axios.put(`/api/v1/auth/order-status/${orderId}`, {
+      const { data } = await axios.put(`https://mern-backend-bu3c.onrender.com/api/v1/auth/order-status/${orderId}`, {
         status: value,
       });
       getOrders();
@@ -93,7 +93,7 @@ const AdminOrders = () => {
                     <div className="row mb-2 p-3 card flex-row" key={p._id}>
                       <div className="col-md-4">
                         <img
-                          src={`/api/v1/product/product-photo/${p._id}`}
+                          src={`https://mern-backend-bu3c.onrender.com/api/v1/product/product-photo/${p._id}`}
                           className="card-img-top"
                           alt={p.name}
                           width="100px"
