e63ba8f9afedfd5833a9bdfd9596b72d4fea1da5
kamsansari007 <92922613+kamsansari007@users.noreply.github.com>
2023-09-11 12:31:49 +0530

    Update Orders.js
    base url

--- src/pages/user/Orders.js
+++ src/pages/user/Orders.js
@@ -10,7 +10,7 @@ const Orders = () => {
   const [auth, setAuth] = useAuth();
   const getOrders = async () => {
     try {
-      const { data } = await axios.get("/api/v1/auth/orders");
+      const { data } = await axios.get("https://mern-backend-bu3c.onrender.com/api/v1/auth/orders");
       console.log("data", data);
       setOrders(data);
     } catch (error) {
@@ -60,7 +60,7 @@ const Orders = () => {
                       <div className="row mb-2 p-3 card flex-row" key={p._id}>
                         <div className="col-md-4">
                           <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
+                            src={`https://mern-backend-bu3c.onrender.com/api/v1/product/product-photo/${p._id}`}
                             className="card-img-top"
                             alt={p.name}
                             width="100px"
