0b694326c4b31a6065f52ea94c55e560c2b94161
kamsansari007 <92922613+kamsansari007@users.noreply.github.com>
2023-09-11 12:21:02 +0530

    Update SearchInput.js
    updated base url

--- src/components/Form/SearchInput.js
+++ src/components/Form/SearchInput.js
@@ -10,7 +10,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `/api/v1/product/search/${values.keyword}`
+        `https://mern-backend-bu3c.onrender.com/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
